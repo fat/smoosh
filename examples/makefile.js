97d08ef179026f5ab220788752f5f0f6b8dd79b3
Dustin Diaz <polvero@gmail.com>
2011-03-05 16:03:48 -0800

    smoosh with object literal plus cleanup

--- examples/makefile.js
+++ examples/makefile.js
@@ -1,4 +1,4 @@
-var smoosh = require('smoosh');
+var smoosh = require('../'); // run example on self and not what's installed from npm
 
 smoosh
   .config('./config.json')
@@ -6,6 +6,17 @@ smoosh
   .run() //runs jshint on full build
   .build() //builds both uncompressed and compressed files
   .analyze() //analyzes all
-   
-//or simply do everything above with
-//smoosh.make('./config.json');
+
+setTimeout(function() {
+  // try as object literal
+  smoosh
+    .make({
+      "VERSION": "0.1",
+      "SOURCES": {
+        "base": [ "./src/header.js", "./src/script.js" ],
+        "secondary": [ "./src/foo.js", "./src/foo.bar.js", "./src/foo.bar.baz.js" ]
+      },
+      "DIST_DIR": "dist",
+      "JSHINT_OPTS": { "boss": true, "forin": true, "browser": true }
+    });
+}, 1000);
